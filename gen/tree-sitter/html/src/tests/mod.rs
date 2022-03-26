3a23e269b639c656dcee32ec45013ecbcb33c9c0
quentinLD <ledilavrecquentin@gmail.com>
2022-03-26 17:40:22 +0100

    propagate hyperast logic extraction for html gen

--- gen/tree-sitter/html/src/tests/mod.rs
+++ gen/tree-sitter/html/src/tests/mod.rs
@@ -43,3 +43,20 @@ fn html_tree_sitter_on_pom() {
     let tree = parser.parse(&text, None).unwrap();
     println!("{:#?}", tree.root_node().to_sexp());
 }
+
+#[test]
+fn hyperAST_on_pom() {
+    
+    let mut parser = Parser::new();
+
+    {
+        let language = unsafe { tree_sitter_html() };
+        parser.set_language(language).unwrap();
+    }
+
+    let path: PathBuf = Path::new("../../../benchmark/pom.xml").to_path_buf();
+    
+    let text = std::fs::read(path).unwrap();
+    let tree = parser.parse(&text, None).unwrap();
+    println!("{:#?}", tree.root_node().to_sexp());
+}
