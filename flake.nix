9938ca49d2abc6eb9feba992c295b859e3607044
Tol0kk <titouan.le.dilavrec@gmail.com>
2024-01-27 22:37:00 +0100

    Better flake (Nix)

--- flake.nix
+++ flake.nix
@@ -4,48 +4,42 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }@inputs:
+  outputs = { self, nixpkgs, flake-utils, rust-overlay, ... }@inputs:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
       in
       {
         devShell = pkgs.mkShell rec {
           buildInputs = with pkgs; [
             clang
             llvmPackages.bintools
-            rustup
+            (rust-bin.fromRustupToolchainFile ./rust-toolchain)
 
-            pkg-config 
-            openssl.dev
+            pkg-config
+            nixfmt
+            cmake
+            trunk
           ];
 
-          RUSTC_VERSION = pkgs.lib.readFile ./rust-toolchain;
-          LIBCLANG_PATH = pkgs.lib.makeLibraryPath [ pkgs.llvmPackages_latest.libclang.lib ];
-          
-          shellHook = ''
-            export PATH=$PATH:''${CARGO_HOME:-~/.cargo}/bin
-            export PATH=$PATH:''${RUSTUP_HOME:-~/.rustup}/toolchains/$RUSTC_VERSION-x86_64-unknown-linux-gnu/bin/
-          '';
+          libraries = with pkgs; [
+            openssl
+            glibc
+          ];
 
-          RUSTFLAGS = (builtins.map (a: ''-L ${a}/lib'') [
-            # add libraries here
-            
-          ]);
-
-          BINDGEN_EXTRA_CLANG_ARGS = 
-            (builtins.map (a: ''-I"${a}/include"'') [
-              pkgs.pkg-config 
-              pkgs.openssl.dev
-              pkgs.glibc.dev 
-            ])
-            ++ [
-              ''-I"${pkgs.llvmPackages_latest.libclang.lib}/lib/clang/${pkgs.llvmPackages_latest.libclang.version}/include"''
-              ''-I"${pkgs.glib.dev}/include/glib-2.0"''
-              ''-I${pkgs.glib.out}/lib/glib-2.0/include/''
-            ];
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath libraries;
         };
       }
     );
