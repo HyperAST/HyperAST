31acd74e18f6e52d85ee09582dfc75fc756e827f
Leo Mangold <leo@leomangold.com>
2025-05-23 09:45:17 +0200

    feat: precompute inputs

--- benchmark_diffs/benches/cd_runtime.rs
+++ benchmark_diffs/benches/cd_runtime.rs
@@ -1,5 +1,7 @@
-use criterion::{Criterion, criterion_group, criterion_main};
-use hyperast_benchmark_diffs::common::run_diff;
+use criterion::{Criterion, SamplingMode, black_box, criterion_group, criterion_main};
+use hyper_diff::algorithms;
+use hyperast::store::SimpleStores;
+use hyperast_benchmark_diffs::{common::run_diff, preprocess::parse_string_pair};
 
 fn diff_benchmark(c: &mut Criterion) {
     env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("trace"))
@@ -21,7 +23,8 @@ fn diff_benchmark(c: &mut Criterion) {
     );
 
     let mut group = c.benchmark_group("change_distiller_comparison");
-    group.sample_size(30);
+    group.sample_size(10);
+    group.sampling_mode(SamplingMode::Flat);
 
     // group.bench_function("HyperDiff Lazy", |b| {
     //     b.iter(|| {
@@ -30,6 +33,28 @@ fn diff_benchmark(c: &mut Criterion) {
     //         }
     //     })
     // });
+
+    group.bench_function("ChangeDistiller Lazy 2 Precomp", |b| {
+        let inputs = test_inputs
+            .iter()
+            .map(|(src, dst)| {
+                let mut stores = SimpleStores::<hyperast_gen_ts_java::types::TStore>::default();
+                let mut md_cache = Default::default();
+                let (src_tr, dst_tr) = parse_string_pair(&mut stores, &mut md_cache, src, dst);
+                (stores, src_tr, dst_tr)
+            })
+            .collect::<Vec<_>>();
+        b.iter(|| {
+            for (stores, src_tr, dst_tr) in &inputs {
+                let res = algorithms::change_distiller_lazy_2::diff(
+                    stores,
+                    &src_tr.local.compressed_node,
+                    &dst_tr.local.compressed_node,
+                );
+                black_box(res);
+            }
+        })
+    });
 
     group.bench_function("ChangeDistiller Lazy 2", |b| {
         b.iter(|| {
@@ -58,5 +83,10 @@ fn diff_benchmark(c: &mut Criterion) {
     group.finish();
 }
 
-criterion_group!(benches, diff_benchmark);
+criterion_group! {
+    name = benches;
+    config = Criterion::default().configure_from_args();
+    targets = diff_benchmark
+}
+// criterion_group!(benches, diff_benchmark);
 criterion_main!(benches);
