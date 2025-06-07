ca98b18f8da1089f7b9d14d6b3c0bf2de756d496
Maciej Mejer <m.j.mejer@student.tudelft.nl>
2025-06-07 16:10:26 +0200

    improve code quality

--- benchmark_diffs/benches/large_bench.rs
+++ benchmark_diffs/benches/large_bench.rs
@@ -10,54 +10,208 @@ use hyper_diff::matchers::heuristic::gt::greedy_subtree_matcher::GreedySubtreeMa
 use hyper_diff::matchers::{Decompressible, Mapper, Mapping};
 use hyper_diff::matchers::heuristic::gt::greedy_bottom_up_matcher::GreedyBottomUpMatcher;
 use hyper_diff::matchers::mapping_store::{DefaultMultiMappingStore, MappingStore, MonoMappingStore, VecStore};
-use hyper_diff::tree::tree_path::CompressedTreePath;
+use hyper_diff::tree::tree_path::{CompressedTreePath, SimpleTreePath, TreePath};
 use hyperast::store::SimpleStores;
-use hyperast::test_utils::simple_tree::TStore;
-use hyperast::types;
+use hyperast_gen_ts_java::types::TStore;
+use hyperast::{types, PrimInt};
 use hyperast::types::{DecompressedFrom, HyperAST, HyperASTShared, NodeId};
 use hyperast_benchmark_diffs::preprocess::parse_string_pair;
 use hyperast_gen_ts_java::legion_with_refs;
-use walkdir::WalkDir;
+use walkdir::{DirEntry, WalkDir};
 use std::io::Write;
+use std::panic::{catch_unwind, AssertUnwindSafe};
+use hyper_diff::actions::action_vec::ActionsVec;
+use hyper_diff::actions::Actions;
+use hyper_diff::actions::script_generator2::{ScriptGenerator, SimpleAction};
+use hyper_diff::decompressed_tree_store::bfs_wrapper::SimpleBfsMapper;
 use hyper_diff::decompressed_tree_store::lazy_post_order::LazyPostOrder;
-use hyper_diff::matchers::heuristic::gt::simple_bottom_up_matcher::SimpleBottomUpMatcher;
+use hyper_diff::matchers::heuristic::gt::simple_bottom_up_matcher3::SimpleBottomUpMatcher3;
 use hyper_diff::matchers::heuristic::gt::xy_bottom_up_matcher::XYBottomUpMatcher;
 use hyperast::store::defaults::NodeIdentifier;
+use hyperast::store::labels::DefaultLabelIdentifier;
+use hyperast_gen_ts_java::legion_with_refs::FNode;
 
-const DATASET: &str = "gh-java/";
-const INPUT_PATH: &str = "elastic-search/1d732dfc1b08deca0f20b467fe4c66f041bb37b5/";
+const ALGORITHM_NAME: &str = "SimpleBottomUpMatcher3";
 
-fn setup_mapper(filename: &str)  {
-
+fn dataset_files() -> Vec<PathBuf> {
+    WalkDir::new("/home/maciek/HyperAST/gt_datasets/datasets/").into_iter()
+        .filter_entry(|e| {
+            !(e.file_name() == "after")
+        })
+        .filter_map(|e| e.ok())
+        .filter(|e| e.file_type().is_file())
+        .map(|e| e.path().to_path_buf())
+        .collect()
 }
-fn diff_benchmark(c: &mut Criterion) {
-    let before_folder = format!("gt_datasets/{}before/", DATASET);
-    let before_folder: &Path = Path::new(&before_folder);
-    let mut result_file = File::create("results.json").unwrap();
-    writeln!(result_file, "[").expect("could not write");
-    fs::remove_dir_all("/home/maciek/HyperAST/target/criterion/large_bench").expect("could not delete old results");
 
-    let mut group = c.benchmark_group("large_bench");
+fn file_id(path: &PathBuf) -> String {
+    let filename: String = path.file_name().unwrap().to_str().unwrap().to_string();
+    let filename = filename[filename.len().saturating_sub(40)..].to_string();
+    let parent_path = path.parent().unwrap().to_string_lossy().to_string();
 
-    for before_entry in WalkDir::new(Path::new(before_folder)).into_iter().filter_map(|e| e.ok()).filter(|e| e.file_type().is_file()) {
-        let before_entry = before_entry.path();
-        let before_path = before_entry.to_string_lossy().to_string();
-        let file_id = before_entry.to_string_lossy().to_string().replace("/", "-");
-        let file_id = &file_id[file_id.len().saturating_sub(50)..];
-        let after_entry = PathBuf::from(before_entry.to_string_lossy().replace("before", "after"));
+    let file_id = format!("{parent_path}/{filename}");
+    let file_id = file_id.replace("/", "-");
+    file_id[file_id.len().saturating_sub(60)..].to_string()
+}
 
-        let before_content = fs::read_to_string(before_entry).unwrap();
-        let after_content = fs::read_to_string(after_entry).unwrap();
+fn compute_metadata(before_entry: &PathBuf) -> (usize, usize, usize, usize, usize) {
+    let after_entry = PathBuf::from(before_entry.to_string_lossy().replace("before", "after"));
 
-        let mut hyperast = SimpleStores::<hyperast_gen_ts_java::types::TStore>::default();
-        let mut md_cache = Default::default();
+    let before_content = fs::read_to_string(before_entry).unwrap();
+    let after_content = fs::read_to_string(after_entry).unwrap();
 
-        let (src_tr, dst_tr) = parse_string_pair(
+    let mut hyperast = SimpleStores::<hyperast_gen_ts_java::types::TStore>::default();
+    let mut md_cache = Default::default();
+
+    let result = catch_unwind(AssertUnwindSafe(|| {
+        parse_string_pair(
             &mut hyperast,
             &mut md_cache,
             black_box(&before_content),
             black_box(&after_content),
+        )
+    }));
+    let (src_tr, dst_tr) = match result {
+        Ok(pair) => pair,
+        Err(_) => {
+            eprintln!("Could not parse {}", before_entry.to_string_lossy().to_string());
+            return (0, 0, 0, 0, 0);
+        }
+    };
+
+    let src = &src_tr.local.compressed_node;
+    let dst = &dst_tr.local.compressed_node;
+
+    let mut mapper: Mapper<_, CDS<_>, CDS<_>, VecStore<_>> = (&hyperast)
+        .decompress_pair(&src, &dst)
+        .into();
+
+    mapper = GreedySubtreeMatcher::<_, _, _, _>::match_it::<DefaultMultiMappingStore<_>>(mapper);
+
+    let matches_before = mapper.mappings().len();
+
+    let mut src_arena = Decompressible::<_, CompletePostOrder<_, u32>>::decompress(&hyperast, &src);
+    let mut dst_arena = Decompressible::<_, CompletePostOrder<_, u32>>::decompress(&hyperast, &dst);
+    let script_mapper = Mapper {
+        hyperast: &hyperast,
+        mapping: Mapping {
+            src_arena,
+            dst_arena,
+            mappings: mapper.mapping.mappings.clone(),
+        }
+    };
+    let script_mapper = script_mapper.map(
+        |x| x,
+        // the dst side has to be traversed in bfs for chawathe
+        |dst_arena| SimpleBfsMapper::with_store(&hyperast, dst_arena),
+    );
+    let script_length_before: ActionsVec<SimpleAction<DefaultLabelIdentifier, SimpleTreePath<u16>, NodeIdentifier>> = ScriptGenerator::compute_actions(&hyperast, &script_mapper.mapping).ok().unwrap();
+    let script_length_before = script_length_before.len();
+
+    mapper = SimpleBottomUpMatcher3::<_, _, _, _>::match_it(mapper);
+
+    let matches_after = mapper.mappings().len();
+
+    let mut src_arena = Decompressible::<_, CompletePostOrder<_, u32>>::decompress(&hyperast, &src);
+    let mut dst_arena = Decompressible::<_, CompletePostOrder<_, u32>>::decompress(&hyperast, &dst);
+    let script_mapper = Mapper {
+        hyperast: &hyperast,
+        mapping: Mapping {
+            src_arena,
+            dst_arena,
+            mappings: mapper.mapping.mappings.clone(),
+        }
+    };
+    let script_mapper = script_mapper.map(
+        |x| x,
+        // the dst side has to be traversed in bfs for chawathe
+        |dst_arena| SimpleBfsMapper::with_store(&hyperast, dst_arena),
+    );
+    let script_length_after: ActionsVec<SimpleAction<DefaultLabelIdentifier, SimpleTreePath<u16>, NodeIdentifier>> = ScriptGenerator::compute_actions(&hyperast, &script_mapper.mapping).ok().unwrap();
+    let script_length_after = script_length_after.len();
+
+    let size_src = mapper.mapping.src_arena.iter().count();
+    let size_dst = mapper.mapping.dst_arena.iter().count();
+    let size = hyperast.node_store.len();
+    let total_size = size_src + size_dst;
+
+    println!("Size: {}, {}, {}", size, size_src, size_dst);
+    println!("Matches: {}, {}", matches_before, matches_after);
+    println!("Script length: {}, {}", script_length_before, script_length_after);
+
+    (total_size, matches_before, matches_after, script_length_before, script_length_after)
+}
+
+fn compile_results() {
+    let mut result_file = File::create("results.json").unwrap();
+    writeln!(result_file, "[").expect("could not write");
+
+    for before_entry in dataset_files() {
+
+        let before_path = before_entry.to_string_lossy().to_string();
+
+        let path = format!("/home/maciek/HyperAST/target/criterion/large_bench/{ALGORITHM_NAME}/{}/new/estimates.json", file_id(&before_entry));
+        let path = Path::new(&path);
+        dbg!(path);
+
+        let (total_size, matches_before, matches_after, script_length_before, script_length_after) = compute_metadata(&before_entry);
+        if let Ok(result) = fs::read_to_string(&path) {
+            writeln!(result_file, "{{
+                \"file_name\": \"{before_path}\",
+                \"size\": {total_size},
+                \"matches_before\": {matches_before},
+                \"script_length_before\": {script_length_before},
+                \"matches_after\": {matches_after},
+                \"script_length_after\": {script_length_after},
+                \"criterion\": {result}
+            }},"
+            ).expect("could not write");
+        } else {
+            println!("Could not read file {}", path.display());
+        }
+    }
+
+    writeln!(result_file, "]").expect("could not write");
+}
+
+fn diff_benchmark(c: &mut Criterion) {
+    let mut group = c.benchmark_group("large_bench");
+
+    dbg!(dataset_files());
+
+    for before_entry in dataset_files() {
+        let path = format!(
+            "/home/maciek/HyperAST/target/criterion/large_bench/{ALGORITHM_NAME}/{}/new/estimates.json",
+            file_id(&before_entry)
         );
+        if Path::new(&path).exists() {
+            println!("Results exists, skipping: {}", path);
+            continue;
+        }
+        
+        let after_entry = PathBuf::from(before_entry.to_string_lossy().replace("before", "after"));
+
+        let before_content = fs::read_to_string(&before_entry).unwrap();
+        let after_content = fs::read_to_string(&after_entry).unwrap();
+
+        let mut hyperast = SimpleStores::<hyperast_gen_ts_java::types::TStore>::default();
+        let mut md_cache = Default::default();
+
+        let result = catch_unwind(AssertUnwindSafe(|| {
+            parse_string_pair(
+                &mut hyperast,
+                &mut md_cache,
+                black_box(&before_content),
+                black_box(&after_content),
+            )
+        }));
+        let (src_tr, dst_tr) = match result {
+            Ok(pair) => pair,
+            Err(_) => {
+                eprintln!("Could not parse {}", before_entry.to_string_lossy().to_string());
+                continue;
+            }
+        };
 
         let src = &src_tr.local.compressed_node;
         let dst = &dst_tr.local.compressed_node;
@@ -69,21 +223,7 @@ fn diff_benchmark(c: &mut Criterion) {
         mapper = GreedySubtreeMatcher::<_, _, _, _>::match_it::<DefaultMultiMappingStore<_>>(mapper);
         let input_mappings = mapper.mapping.mappings.clone();
 
-        let matches_before = mapper.mappings().len();
-
-        mapper = XYBottomUpMatcher::<_, _, _, _>::match_it(mapper);
-
-        let matches_after = mapper.mappings().len();
-
-        let size_src = mapper.mapping.src_arena.iter().count();
-        let size_dst = mapper.mapping.dst_arena.iter().count();
-        let size = hyperast.node_store.len();
-        let total_size = size_src + size_dst;
-
-        println!("Size: {}, {}, {}", size, size_src, size_dst);
-        println!("Matches: {}, {}", matches_before, matches_after);
-
-        group.bench_function(BenchmarkId::from_parameter(&file_id), |b| {
+        group.bench_function(BenchmarkId::new(ALGORITHM_NAME, file_id(&before_entry)), |b| {
             b.iter_custom(|iters| {
                 let mut total = Duration::ZERO;
                 for _ in 0..iters {
@@ -99,35 +239,18 @@ fn diff_benchmark(c: &mut Criterion) {
                     };
 
                     let start = Instant::now();
-                    black_box(XYBottomUpMatcher::<_, _, _, _>::match_it(black_box(mapper)));
+                    SimpleBottomUpMatcher3::<_, _, _, _>::match_it(black_box(mapper));
                     total += start.elapsed();
                 }
 
                 total
             })
         });
-
-        let path = format!("/home/maciek/HyperAST/target/criterion/large_bench/{}/new/estimates.json", &file_id);
-        let path = Path::new(&path);
-        dbg!(path);
-        while !path.exists() {
-            sleep(Duration::from_millis(100));
-        }
-        let result = fs::read_to_string(&path).unwrap();
-
-        writeln!(result_file, "{{
-                \"file_name\": \"{before_path}\",
-                \"size\": {total_size},
-                \"matches_before\": {matches_before},
-                \"matches_after\": {matches_after},
-                \"criterion\": {result}
-            }},"
-        ).expect("could not write");
     }
 
-    writeln!(result_file, "]").expect("could not write");
-
     group.finish();
+
+    compile_results();
 }
 
 criterion_group!{
@@ -142,3 +265,5 @@ criterion_main!(benches);
 
 #[allow(type_alias_bounds)]
 type CDS<HAST: HyperASTShared> = Decompressible<HAST, CompletePostOrder<HAST::IdN, u32>>;
+
+
