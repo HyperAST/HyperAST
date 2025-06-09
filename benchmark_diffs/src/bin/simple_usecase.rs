aa1359c7a2fe5c0ddf16024d13210b1e2fbcced3
quentin Le Dilavrec <ledilavrecquentin@gmail.com>
2025-06-09 11:24:31 +0200

    update simple diff benchmark to latest preprocessing approach

--- benchmark_diffs/src/bin/simple_usecase.rs
+++ benchmark_diffs/src/bin/simple_usecase.rs
@@ -1,6 +1,6 @@
 // window of one is just consecutive commits
 
-use hyperast_vcs_git::preprocessed::PreProcessedRepository;
+use hyperast_vcs_git::{multi_preprocessed::PreProcessedRepositories, processing::RepoConfig};
 use std::{
     fs::File,
     io::{BufWriter, Write},
@@ -30,9 +30,15 @@ fn main() {
             Some(PathBuf::from_str(x).unwrap())
         }
     });
-    let whol = args.get(5).map_or(false, |x| x == "--whole");
+    let whol = args.get(5).is_some_and(|x| x == "--whole");
 
-    let preprocessed = PreProcessedRepository::new(&repo_name);
+    let mut preprocessed = PreProcessedRepositories::default();
+    let mut repo = repo_name.split("/");
+    let user = repo.next().unwrap();
+    let name = repo.next().unwrap();
+    let repo = hyperast_vcs_git::git::Forge::Github.repo(user, name);
+    let repo = preprocessed.register_config(repo, RepoConfig::JavaMaven);
+    let repo = repo.fetch();
     // let (before, after) = (
     //         // "e01840e86db739374c9c4eb84a948b24ca017d8e", // parent
     //         "cf626243f627cca3d52ff073ebc53eca9595d4b5", // git log --pretty=format:"%H" | head
@@ -47,17 +53,23 @@ fn main() {
     //     );
     // whole();
     if whol {
-        whole(preprocessed, before, after, out);
+        whole(preprocessed, repo, before, after, out);
     } else {
-        inc(preprocessed, before, after, out);
+        inc(preprocessed, repo, before, after, out);
     }
 }
 
 /// incrementally build each commits and compute diffs
 /// ie. interlace building a commit and computing diff with its child commit
 /// nb. 2 commits need to be build before doing the first diff
-fn inc(mut preprocessed: PreProcessedRepository, before: &str, after: &str, out: Option<PathBuf>) {
-    let batch_id = format!("{}:({},{})", &preprocessed.name, before, after);
+fn inc(
+    mut preprocessed: PreProcessedRepositories,
+    repo: hyperast_vcs_git::processing::ConfiguredRepo2,
+    before: &str,
+    after: &str,
+    out: Option<PathBuf>,
+) {
+    let batch_id = format!("{}:({},{})", &repo.spec.url(), before, after);
     dbg!(batch_id);
     // let mu = memusage_linux();
     // let hyperast_size = memusage_linux() - mu;
@@ -92,13 +104,10 @@ fn inc(mut preprocessed: PreProcessedRepository, before: &str, after: &str, out:
         if curr == before {
             break;
         }
-        let processing_ordered_commits = preprocessed.pre_process_with_limit(
-            &mut hyperast_vcs_git::git::fetch_github_repository(&preprocessed.name),
-            "",
-            &curr,
-            "",
-            2,
-        );
+        let processing_ordered_commits = preprocessed
+            .processor
+            .pre_process_with_limit(&repo, "", &curr, 2)
+            .unwrap();
         let oid_src = processing_ordered_commits[1];
         let oid_dst = processing_ordered_commits[0];
         assert_eq!(curr, oid_dst.to_string());
@@ -106,15 +115,15 @@ fn inc(mut preprocessed: PreProcessedRepository, before: &str, after: &str, out:
 
         let stores = &preprocessed.processor.main_stores;
 
-        let commit_src = preprocessed.commits.get_key_value(&oid_src).unwrap();
-        let time_src = commit_src.1.processing_time();
-        let src_tr = commit_src.1.ast_root;
+        let commit_src = preprocessed.get_commit(&repo.config, &oid_src).unwrap();
+        let time_src = commit_src.processing_time();
+        let src_tr = commit_src.ast_root;
         use hyperast::types::WithStats;
         let src_s = stores.node_store.resolve(src_tr).size();
 
-        let commit_dst = preprocessed.commits.get_key_value(&oid_dst).unwrap();
-        let time_dst = commit_dst.1.processing_time();
-        let dst_tr = commit_dst.1.ast_root;
+        let commit_dst = preprocessed.get_commit(&repo.config, &oid_dst).unwrap();
+        let time_dst = commit_dst.processing_time();
+        let dst_tr = commit_dst.ast_root;
         let dst_s = stores.node_store.resolve(dst_tr).size();
 
         let hyperast = hyperast_vcs_git::no_space::as_nospaces2(stores);
@@ -133,8 +142,8 @@ fn inc(mut preprocessed: PreProcessedRepository, before: &str, after: &str, out:
                 "{oid_src}/{oid_dst},{},{},{},{},{},{},{},{},{}",
                 src_s,
                 dst_s,
-                Into::<isize>::into(&commit_src.1.memory_used()),
-                Into::<isize>::into(&commit_dst.1.memory_used()),
+                Into::<isize>::into(&commit_src.memory_used()),
+                Into::<isize>::into(&commit_dst.memory_used()),
                 time_src,
                 time_dst,
                 summarized_lazy.mappings,
@@ -154,21 +163,20 @@ fn inc(mut preprocessed: PreProcessedRepository, before: &str, after: &str, out:
 
 /// build the commits then compute diffs
 fn whole(
-    mut preprocessed: PreProcessedRepository,
+    mut preprocessed: PreProcessedRepositories,
+    repo: hyperast_vcs_git::processing::ConfiguredRepo2,
     before: &str,
     after: &str,
     out: Option<PathBuf>,
 ) {
     let window_size = 2;
-    let batch_id = format!("{}:({},{})", &preprocessed.name, before, after);
+    let batch_id = format!("{}:({},{})", &repo.spec.url(), before, after);
     let mu = memusage_linux();
-    let processing_ordered_commits = preprocessed.pre_process_with_limit(
-        &mut hyperast_vcs_git::git::fetch_github_repository(&preprocessed.name),
-        before,
-        after,
-        "",
-        10,
-    );
+
+    let processing_ordered_commits = preprocessed
+        .processor
+        .pre_process_with_limit(&repo, before, after, 10)
+        .unwrap();
     let hyperast_size = memusage_linux() - mu;
     log::warn!("hyperAST size: {}", hyperast_size);
     log::warn!("batch_id: {batch_id}");
@@ -178,8 +186,8 @@ fn whole(
     let mu = mu - memusage_linux();
     log::warn!("cache size: {mu}");
     log::warn!(
-        "commits ({}): {:?}",
-        preprocessed.commits.len(),
+        "commits: {:?}",
+        // preprocessed.commit_count(&repo.config),
         processing_ordered_commits
     );
     let mut buf = out
@@ -204,14 +212,14 @@ fn whole(
             let stores = &preprocessed.processor.main_stores;
 
             use hyperast::types::WithStats;
-            let commit_src = preprocessed.commits.get_key_value(&oid_src).unwrap();
-            let time_src = commit_src.1.processing_time();
-            let src_tr = commit_src.1.ast_root;
+            let commit_src = preprocessed.get_commit(&repo.config, &oid_src).unwrap();
+            let time_src = commit_src.processing_time();
+            let src_tr = commit_src.ast_root;
             let src_s = stores.node_store.resolve(src_tr).size();
 
-            let commit_dst = preprocessed.commits.get_key_value(&oid_dst).unwrap();
-            let time_dst = commit_dst.1.processing_time();
-            let dst_tr = commit_dst.1.ast_root;
+            let commit_dst = preprocessed.get_commit(&repo.config, oid_dst).unwrap();
+            let time_dst = commit_dst.processing_time();
+            let dst_tr = commit_dst.ast_root;
             let dst_s = stores.node_store.resolve(dst_tr).size();
 
             let hyperast = hyperast_vcs_git::no_space::as_nospaces2(stores);
@@ -229,8 +237,8 @@ fn whole(
                     "{oid_src}/{oid_dst},{},{},{},{},{},{},{},{},{}",
                     src_s,
                     dst_s,
-                    Into::<isize>::into(&commit_src.1.memory_used()),
-                    Into::<isize>::into(&commit_dst.1.memory_used()),
+                    Into::<isize>::into(&commit_src.memory_used()),
+                    Into::<isize>::into(&commit_dst.memory_used()),
                     time_src,
                     time_dst,
                     summarized_lazy.mappings,
